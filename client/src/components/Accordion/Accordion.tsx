0583cfdbca975712e9012309efbb3ba245ebfbfe
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2021-09-16 22:03:14 -0500

    refactor accordion animation

--- client/src/components/Accordion/Accordion.tsx
+++ client/src/components/Accordion/Accordion.tsx
@@ -1,8 +1,9 @@
 import { useToggle } from '@alehechka/react-hooks';
 import { ReactNode } from 'react';
 import { FC } from 'react';
-import { Dropdown, Wrap } from './Accordion.styled';
-import { BsChevronDown, BsChevronUp } from 'react-icons/bs';
+import { Dropdown, dropIn, Wrap } from './Accordion.styled';
+import { BsChevronDown } from 'react-icons/bs';
+import { AnimatePresence, motion } from 'framer-motion';
 
 interface Props {
 	label: ReactNode;
@@ -13,11 +14,25 @@ const Accordion: FC<Props> = ({ label, children }) => {
 
 	return (
 		<>
-			<Wrap onClick={toggleOpen} active={isOpen}>
+			<Wrap onClick={toggleOpen} initial={false} animate={{ backgroundColor: isOpen ? '#ccc' : '#eee' }}>
 				{label}
-				<span>{isOpen ? <BsChevronUp /> : <BsChevronDown />}</span>
+				<motion.span initial={false} animate={{ scaleY: isOpen ? -1 : 1 }}>
+					<BsChevronDown />
+				</motion.span>
 			</Wrap>
-			{isOpen && <Dropdown isOpen={isOpen}>{children}</Dropdown>}
+			<AnimatePresence initial={false}>
+				{isOpen && (
+					<Dropdown
+						variants={dropIn}
+						initial='collapsed'
+						animate='open'
+						exit='collapsed'
+						transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
+					>
+						{children}
+					</Dropdown>
+				)}
+			</AnimatePresence>
 		</>
 	);
 };
